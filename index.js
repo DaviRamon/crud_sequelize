fd3aa87054bca98571c55ae3ecd0400b2c846927
Davi Ramon <eudavi.ramon@gmail.com>
2022-01-05 20:32:40 -0300

    criado a toda para registrar um usuario novo.

--- index.js
+++ index.js
@@ -21,6 +21,33 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
+
+app.get('/users/create', (req, res) =>{
+    res.render('adduser')
+})
+
+
+app.post('/users/create', async(req, res) => {
+
+    const name = req.body.name
+    const occupation = req.body.occupation
+    let newsletter = req.body.newsletter // o checkbox já vem ticado [ON]. o boolean vem true. 
+    
+    if(newsletter === 'on') {
+        newsletter = true
+    } else {
+        newsletter = false
+    }
+    
+    console.log(req.body)
+
+    await User.create({name, occupation, newsletter})
+    res.redirect('/')
+
+
+})
+
+
 app.get('/',  (req, res) => {
     res.render('home')
 })
